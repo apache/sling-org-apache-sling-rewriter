7be7fd3d9d3aa7009f7bb8a85c9498085d278462
Oliver Lietz <olli@apache.org>
2019-06-20 16:05:37 +0200

    SLING-8524 Make testing compliant with Java 9 and higher
    * Use Sling Bundle Parent 35
    * Update Pax Exam to 4.13.1
    * Update Testing PaxExam to 3.0.0
    * Update Felix Framework to 6.0.3
    * Update Sling Mock (JUnit4) to 2.3.12
    * Update Logging Mock to 2.0.0

--- src/test/java/org/apache/sling/rewriter/it/tests/RewriterTestSupport.java
+++ src/test/java/org/apache/sling/rewriter/it/tests/RewriterTestSupport.java
@@ -18,32 +18,57 @@
  */
 package org.apache.sling.rewriter.it.tests;
 
-import static org.apache.sling.testing.paxexam.SlingOptions.slingCommonsHtml;
-import static org.apache.sling.testing.paxexam.SlingOptions.slingCommonsMetrics;
-import static org.apache.sling.testing.paxexam.SlingOptions.slingLaunchpadOakTar;
-import static org.apache.sling.testing.paxexam.SlingOptions.slingScriptingJavascript;
-import static org.ops4j.pax.exam.CoreOptions.composite;
-import static org.ops4j.pax.exam.CoreOptions.junitBundles;
-import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
+import javax.inject.Inject;
 
+import org.apache.sling.api.servlets.ServletResolver;
+import org.apache.sling.resource.presence.ResourcePresence;
 import org.apache.sling.testing.paxexam.TestSupport;
 import org.ops4j.pax.exam.Configuration;
 import org.ops4j.pax.exam.Option;
 import org.ops4j.pax.exam.ProbeBuilder;
 import org.ops4j.pax.exam.TestProbeBuilder;
+import org.ops4j.pax.exam.util.Filter;
+import org.osgi.service.http.HttpService;
+
+import static org.apache.sling.testing.paxexam.SlingOptions.slingCommonsHtml;
+import static org.apache.sling.testing.paxexam.SlingOptions.slingQuickstartOakTar;
+import static org.apache.sling.testing.paxexam.SlingOptions.slingResourcePresence;
+import static org.apache.sling.testing.paxexam.SlingOptions.slingScriptingJavascript;
+import static org.ops4j.pax.exam.CoreOptions.composite;
+import static org.ops4j.pax.exam.CoreOptions.junitBundles;
+import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
+import static org.ops4j.pax.exam.cm.ConfigurationAdminOptions.factoryConfiguration;
 
 public abstract class RewriterTestSupport extends TestSupport {
+
+    @Inject
+    protected ServletResolver servletResolver;
+
+    @Inject
+    protected HttpService httpService;
+
+    @Inject
+    @Filter(value = "(path=/content/rewriter)")
+    private ResourcePresence resourcePresence;
 
     @Configuration
     public Option[] configuration() {
         return new Option[]{
             baseConfiguration(),
-            launchpad(),
+            quickstart(),
             // Sling Rewriter
             testBundle("bundle.filename"),
+            slingCommonsHtml(),
+            factoryConfiguration("org.apache.sling.serviceusermapping.impl.ServiceUserMapperImpl.amended")
+                .put("user.mapping", new String[]{"org.apache.sling.rewriter=sling-readall"})
+                .asOption(),
+            factoryConfiguration("org.apache.sling.resource.presence.internal.ResourcePresenter")
+                .put("path", "/content/rewriter")
+                .asOption(),
             // testing
             mavenBundle().groupId("org.jsoup").artifactId("jsoup").versionAsInProject(),
             mavenBundle().groupId("org.apache.servicemix.bundles").artifactId("org.apache.servicemix.bundles.hamcrest").versionAsInProject(),
+            slingResourcePresence(),
             junitBundles()
         };
     }
@@ -52,18 +77,16 @@ public abstract class RewriterTestSupport extends TestSupport {
     public TestProbeBuilder probeConfiguration(final TestProbeBuilder testProbeBuilder) {
         testProbeBuilder.setHeader("Sling-Initial-Content",
             "apps/esp;path:=/apps/esp;overwrite:=true;uninstall:=true" + "," +
-            "content;path:=/content;overwrite:=true;uninstall:=true"
+                "content;path:=/content;overwrite:=true;uninstall:=true"
         );
         return testProbeBuilder;
     }
 
-    protected Option launchpad() {
+    protected Option quickstart() {
         final int httpPort = findFreePort();
         final String workingDirectory = workingDirectory();
         return composite(
-            slingLaunchpadOakTar(workingDirectory, httpPort),
-            slingCommonsHtml(),
-            slingCommonsMetrics(),
+            slingQuickstartOakTar(workingDirectory, httpPort),
             slingScriptingJavascript()
         );
     }
